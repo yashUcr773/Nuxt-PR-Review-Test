57310b64551000702fd5adc31b8cbc34008b384e
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-10-08 11:21:13 +0530

    seed app

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Home></Home>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Home from './components/Home.vue';
+</script>
---
+++ components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="home">
+        <h1>Welcome to My Nuxt App</h1>
+        <p>This is the homepage of your Nuxt app.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style scoped>
+.home {
+    text-align: center;
+    margin-top: 50px;
+}
+
+h1 {
+    font-size: 3rem;
+    color: #333;
+}
+
+p {
+    font-size: 1.5rem;
+    margin-top: 20px;
+}
+</style>
